ce2773f9500bc6d18784cdd8e7e7d3c6ca0f3ee2
Mohamatam <102506744+Mohamatam@users.noreply.github.com>
2022-04-18 16:10:51 +0530

    Add files via upload

---
+++ Param.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    internal class Param
+    {
+
+        public static void UseParams(params int[] list)
+        {
+            int sum = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+
+                sum += list[i];
+            }
+            Console.WriteLine("the sum of all numbers is {0}", sum);
+        }
+        static void Main()
+        {
+            UseParams(1, 2, 3, 6, 8, 56);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2, result = 0;
+            Console.WriteLine("Enter the first number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter an operator (+,-,*,/): ");
+            char op = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("You enter the wrong operator");
+                    break;
+            }
+            Console.WriteLine(num1 + " " + op + " " + num2 + " " + result);
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
