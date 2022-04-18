9bf0f1e98c1860a72c25fd2e111ef53ef9357f10
Mohamatam <102506744+Mohamatam@users.noreply.github.com>
2022-04-18 18:45:42 +0530

    Add files via upload

---
+++ Assignment5/EmployeeDetails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class EmployeeDetails
+    {
+
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSal { get; set; }
+        public string EmpLoc { get; set; }
+
+
+    }
+
+    internal class ArrayListSample
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList arrayList = new ArrayList()
+            {
+                    new EmployeeDetails{EmpId=1,EmpName="suresh",EmpSal=40000,EmpLoc="banglore"},
+                    new EmployeeDetails{EmpId=2,EmpName="Haritha",EmpSal=50000,EmpLoc="hyderabad"},
+                    new EmployeeDetails{EmpId=3,EmpName="prasanna",EmpSal=40000,EmpLoc="pune"},
+                    new EmployeeDetails{EmpId=4,EmpName="sai",EmpSal=30000,EmpLoc="Chennai"},
+            };
+            foreach (EmployeeDetails emp in arrayList)
+            {
+                Console.WriteLine(emp.EmpId + " " + emp.EmpName + " " + emp.EmpSal + " " + emp.EmpLoc);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Assignment5/ListEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class ListEmployee
+    {
+
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            list.Add("priya");
+            list.Add("Haritha");
+            list.Add("siva");
+            list.Add("Prasanna");
+            list.Add("karthik");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("List of employees are " + ':' + list[i]);
+            }
+            Console.WriteLine(" Total number of employees is " + ':' + list.Count);
+            Console.ReadLine();
+        }
+    }
+}
