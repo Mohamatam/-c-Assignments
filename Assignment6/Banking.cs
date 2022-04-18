0abacac885fef9619a369ed3a1559f874430e735
Mohamatam <102506744+Mohamatam@users.noreply.github.com>
2022-04-18 19:34:07 +0530

    Add files via upload

---
+++ Assignment6/Banking.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class Banking
+    {
+
+        public int AccNumber;
+        public string CustomerName;
+        public double Balance;
+
+        public Banking(int AccNumber, String CustomerName, double Balance)
+        {
+
+            this.AccNumber = AccNumber;
+            this.CustomerName = CustomerName;
+            this.Balance = Balance;
+        }
+
+
+        public void Withdraw(double amount)
+        {
+            Console.WriteLine("enter the amount withdraw");
+            Console.ReadLine();
+
+
+            if (this.Balance > 400)
+            {
+                if (amount == 300 || amount == 500 || amount == 5000)
+                {   
+                    Console.WriteLine("1.Savings Account");
+                    Console.WriteLine("2.Current Account");
+                    Console.Read();
+                    Console.WriteLine("Your transaction is processing...");
+                    double finalbalance = Balance + amount;
+                    Console.WriteLine(finalbalance + " : here is your balance after debiting the money, If not done by you please inform the local bank officials ");
+                }
+                else
+                {
+                    Console.WriteLine("Oops!! something went wrong");
+                }
+
+            }
+            else if (Balance < 500)
+            {
+                Console.WriteLine("Your balance is less than 500rs sorry we cannot proceed");
+            }
+            else
+            {
+                Console.WriteLine("Your Session has been Expired!!");
+            }
+
+        }
+        public void Deposit(double amt)
+                {
+                    Console.WriteLine("Enter the amount you want to deposit ");
+                    Console.Read();
+                    if (amt >= 500)
+                    {
+                        Console.WriteLine("You have entered valid number");
+                        Console.WriteLine("Thank you ! your deposition is successful");
+                        double dep = Balance + amt;
+                  
+
+                    }
+                    else if (amt < 400)
+                    {
+                        Console.WriteLine("please deposit more than 400Rs");
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid number");
+                    }
+                }
+            }
+    internal class WithdrawDeposit
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("welcome to appliaction");
+                Console.ReadKey();
+                Banking ac1 = new Banking(76549, "Haritha", 1000000);
+                ac1.Withdraw(400);
+
+
+            }
+        }
+    }
---
+++ Assignment6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class Program
+    {
+        public delegate void Employee(); //declaring Unicast delegate
+        internal class UniCastDelegate
+        {
+            static void Main(string[] args)
+            {
+                Manager manager = new Manager();
+                Employee employeeDelegate = new Employee(manager.Display);
+                employeeDelegate.Invoke();
+            }
+        }
+        public class Manager
+        {
+            private int Id;
+            private string managerName;
+            private string companyName;
+            private string Salary;
+         
+
+            public Manager()
+            {
+                Console.Write("Enter the  id : ");
+                this.Id = (Convert.ToInt32(Console.ReadLine()));
+                Console.Write("Enter the manager name : ");
+                this.managerName = Console.ReadLine();
+
+                Console.Write("Enter the Company Name : ");
+                this.companyName = Console.ReadLine();
+
+                Console.Write("Enter the manager salary : ");
+                this.Salary = Console.ReadLine();
+
+            }
+
+            public void Display()
+            {
+                Console.WriteLine(" Id : {0}", Id);
+                Console.WriteLine("Manager Name : {0}", managerName);
+                Console.WriteLine("Manager Company Name : {0}", companyName);
+
+                Console.WriteLine("Salary : {0}", Salary);
+
+               
+            }
+        
+        }
+    }
+}
