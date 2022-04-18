a68a490f501ff0b59548b173c6e4cf47fc913863
Mohamatam <102506744+Mohamatam@users.noreply.github.com>
2022-04-18 16:13:54 +0530

    Add files via upload

---
+++ Assignments1/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    internal class Circle
+    {
+        static void Main()
+        {
+            Double pi = 3.14;
+            Console.WriteLine("enter the radius of circle :");
+            Double r = Convert.ToDouble(Console.ReadLine());
+            Double Area = pi * r * r;
+            Console.WriteLine("The approximated area of circle is {0}", Area);
+        }
+    }
+}
---
+++ Assignments1/Swaping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    internal class Swaping
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine("first number is : {0}", num1);
+            Console.WriteLine("second number is : {0}", num2);
+        }
+    }
+}
